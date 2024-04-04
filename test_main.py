4930cfe44ab29a7f18e37a4b5b7e501d49c0d859
Vadim Gera <horsespb@yandex.ru>
2024-04-05 00:38:43 +0300

    Тест

---
+++ test_main.py
@@ -0,0 +1,21 @@
+from fastapi.testclient import TestClient
+import time
+from main import app
+
+client = TestClient(app)
+
+
+def test_create_task():
+    response = client.post("/task", json={"duration": 1})
+    assert response.status_code == 200
+    task_id = response.json()["task_id"]
+
+    response = client.get(f"/task/{task_id}")
+    assert response.status_code == 200
+    assert response.json() == {"status": "running"}
+
+    time.sleep(3)
+
+    response = client.get(f"/task/{task_id}")
+    assert response.status_code == 200
+    # assert response.json() == {"status": "done"}
